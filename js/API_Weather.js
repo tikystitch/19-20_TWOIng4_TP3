820599e03bbaa36ebd8bd96908de7ea8e7a52ce6
tikystitch <36538825+tikystitch@users.noreply.github.com>
2019-10-26 02:19:22 +0200

    1-2-3 done

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -3,30 +3,56 @@
 // Clé api
 const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 // Url API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_URL_Today = "https://api.openweathermap.org/data/2.5/weather";  //today
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
+//URL API
+//https://api.openweathermap.org/data/2.5/forecast/daily?q=paris&units=metric&cnt=4&appid=4081444b7b90198136fefe6ed4ccf35b
+const API_URL_3 = "https://api.openweathermap.org/data/2.5/forecast/daily" //3jours
+
 
 class API_WEATHER{
-  constructor(city){
+    constructor(city) {
     // Si la ville n'est pas définit alors la ville par défault est Paris
     if(city === undefined){
       city = "paris";
-    }
+        }
     this.city = city;
   }
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
   fetchTodayForecast(){
-    return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+      return axios
+        //get data throught API 
+        //Pour trois jours: &cnt=4&appid=4081444b7b90198136fefe6ed4ccf35b
+          .get(`${API_URL_Today}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+      crossdomain: true
+    })
+    }
+   
+  fetchThreeForecast(){
+      return axios
+        //get data throught API 
+        //Pour 7 jours: ?q=paris &units=metric &cnt=3& appid=4081444b7b90198136fefe6ed4ccf35b
+          .get(`${API_URL_3}?q=${this.city}&units=metric&cnt=3&appid=${API_KEY}`, {
       crossdomain: true
     })
   }
+
+
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
-  }
+    }
+
+  //Set city
+  setCity() {
+      this.city = document.getElementById('city-input').value;
+      alert(this.city); 
+    }
+
+
 }
--- js/script.js
+++ js/script.js
@@ -11,12 +11,13 @@ function start() {
       // Récupère la donnée d'une API
       const data = response.data;
 
+
       // On récupère l'information principal
       const main = data.weather[0].main;
       const description = data.weather[0].description;
       const temp = data.main.temp;
       const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
-
+    
       // Modifier le DOM
       document.getElementById('today-forecast-main').innerHTML = main;
       document.getElementById('today-forecast-more-info').innerHTML = description;
@@ -27,5 +28,46 @@ function start() {
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
-    });
+        })
+}
+
+function newCity() {
+    // Création de l'objet apiWeather
+    const apiWeather = new API_WEATHER();
+    apiWeather.setCity();
+
+}
+ 
+function getThreeDayForecast() {
+
+    // Création de l'objet apiWeather
+    const apiWeather = new API_WEATHER();
+    // Appel de la fonction fetchTodayForecast
+
+    apiWeather
+        .fetchThreeForecast()
+        .then(function (response) {
+            // Récupère la donnée d'une API
+            const data = response.data;
+
+           
+
+            // On récupère l'information principal
+            const main = data.list[0].weather[0].main;
+            const description = data.list[0].weather[0].description;
+            const temp = data.list[0].temp.day;
+            const icon = apiWeather.getHTMLElementFromIcon(data.list[0].weather[0].icon);
+       
+
+            // Modifier le DOM
+            document.getElementById('today-forecast-main').innerHTML = main;
+            document.getElementById('today-forecast-more-info').innerHTML = description;
+            document.getElementById('icon-weather-container').innerHTML = icon;
+            document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+             
+        })
+        .catch(function (error) {
+            // Affiche une erreur
+            console.error(error);
+        })
 }
