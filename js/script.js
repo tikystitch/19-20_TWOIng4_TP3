5199967530ac76730a33db760d607fc20f61be1d
tikystitch <36538825+tikystitch@users.noreply.github.com>
2019-10-26 03:41:59 +0200

    3days done

--- js/script.js
+++ js/script.js
@@ -53,23 +53,44 @@ function getThreeDayForecast() {
             // Récupère la donnée d'une API
             const data = response.data;
 
+            /*
             //Pour filtrer: 
+            const filtreSup3 = data.filter(list => {
+                if (list > 3) return true; 
+                return false
+            })
+            */
 
+            //On prend uniquement les 3 premiers jours
+            for (let pas = 0; pas < 3; pas++) {
+                // On récupère l'information principal
+                const main = data.list[pas].weather[0].main;
+                const description = data.list[pas].weather[0].description;
+                const temp = data.list[pas].temp.day;
+                const icon = apiWeather.getHTMLElementFromIcon(data.list[pas].weather[0].icon);
 
-
-            // On récupère l'information principal
-            const main = data.list[0].weather[0].main;
-            const description = data.list[0].weather[0].description;
-            const temp = data.list[0].temp.day;
-            const icon = apiWeather.getHTMLElementFromIcon(data.list[0].weather[0].icon);
-       
-
-            // Modifier le DOM
-            document.getElementById('tomorrow-forecast-main').innerHTML = main;
-            document.getElementById('tomorrow-forecast-more-info').innerHTML = description;
-            document.getElementById('icon-weather-container-deux').innerHTML = icon;
-            document.getElementById('tomorrow-forecast-temp').innerHTML = `${temp}°C`;
-             
+                if (pas === 0) {
+                    // Modifier le DOM
+                    document.getElementById('tomorrow-forecast-main').innerHTML = main;
+                    document.getElementById('tomorrow-forecast-more-info').innerHTML = description;
+                    document.getElementById('tomorrow-icon-weather-container').innerHTML = icon;
+                    document.getElementById('tomorrow-forecast-temp').innerHTML = `${temp}°C`;
+                }
+                if (pas === 1) {
+                    // Modifier le DOM
+                    document.getElementById('AD-forecast-main').innerHTML = main;
+                    document.getElementById('AD-forecast-more-info').innerHTML = description;
+                    document.getElementById('AD-icon-weather-container').innerHTML = icon;
+                    document.getElementById('AD-forecast-temp').innerHTML = `${temp}°C`;
+                }
+                if (pas === 2) {
+                    // Modifier le DOM
+                    document.getElementById('AAD-forecast-main').innerHTML = main;
+                    document.getElementById('AAD-forecast-more-info').innerHTML = description;
+                    document.getElementById('AAD-icon-weather-container').innerHTML = icon;
+                    document.getElementById('AAD-forecast-temp').innerHTML = `${temp}°C`;
+                }
+            }
         })
         .catch(function (error) {
             // Affiche une erreur
